4634a9dfc7e0928c98eb8e63eb6eaa42173c21ca
Paul Ambrose <pambrose@mac.com>
2017-01-21 00:57:37 -0500

    Move utils to common package

--- python/sys_watcher.py
+++ python/sys_watcher.py
@@ -6,8 +6,8 @@ import socket
 
 import paho.mqtt.client as paho
 
-from utils import LOGGING_ARGS
-from utils import mqtt_broker_info
+from common_constants import LOGGING_ARGS
+from common_utils import mqtt_broker_info
 
 
 def on_connect(client, userdata, flags, rc):
